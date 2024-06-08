30da5262b33f0e0a260ab9b23abdd203bd61bc4e
statskiy-sovetnik <ivangladkikh3@gmail.com>
2024-06-08 15:00:38 +0500

    feat: DPX contracts + basic pls tokens

---
+++ contracts/FeeCollector.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract FeeCollector is Ownable {
+  constructor(address _gov) {
+    transferOwnership(_gov);
+  }
+
+  function collect(IERC20 _token) external onlyOwner {
+    if (address(this).balance != 0) {
+      payable(owner()).transfer(address(this).balance);
+      emit FeeCollected(address(0), address(this).balance);
+    }
+
+    uint256 _amt = _token.balanceOf(address(this));
+    _token.transfer(owner(), _amt);
+    emit FeeCollected(address(_token), _amt);
+  }
+
+  event FeeCollected(address _token, uint256 _amt);
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+      enabled: true,
+      runs: 1000
+    },
+    outputSelection: {
+      "*": {
+        "*": [
+          "evm.bytecode",
+          "evm.deployedBytecode",
+          "devdoc",
+          "userdoc",
+          "metadata",
+          "abi"
+        ]
+      }
+    },
+    "libraries": {}
+    }
+  },
+};
+
+export default config;
